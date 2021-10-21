f322857139124b855a55c232211f524ab6a5113c
Dozie <64153709+Doziee@users.noreply.github.com>
2021-10-21 19:28:30 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Cedric diverse</Text>
+      <Text>Emma</Text>
+      <Text>Cia</Text>
+      <Text>Sly</Text>
+      <Text>Godson</Text>
+      <Text>Hadi</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
