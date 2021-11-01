f756b6ef6086dd2cf901e66663de34c44ae9202e
Dozie <64153709+Doziee@users.noreply.github.com>
2021-11-01 20:04:53 +0000

    Updated

--- App.js
+++ App.js
@@ -1,34 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from "react";
-import { Text, View, StyleSheet } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './screens/Login';
-import Home from './screens/Home';
-import Cart from './screens/Cart';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import Cart from "./screens/Cart";
+
 
 export default function App() {
-    const MainNavigator = createStackNavigator();
-
+  const MainNavigator = createStackNavigator();
   return (
-    <View>
-        <NavigationContainer>
-            <MainNavigator>
-                <Stack.Screen name = "Home" component={Home} />
-                <Stack.Screen name = "Login" component={Login} />
-                <Stack.Screen name = "Cart" component={Cart} />
-            </MainNavigator>
-
-        </NavigationContainer>
+    <View style={{flex: 1}}>
+     <NavigationContainer>
+       <MainNavigator.Navigator 
+       screenOptions = {{
+        headerShown: false
+      }}
+       initialRouteName="Login"
+       >
+         <MainNavigator.Screen name="Login" component = {Login}/>
+         <MainNavigator.Screen name="Home" component = {Home}/> 
+         <MainNavigator.Screen name="Cart list" component = {Cart}/>
+       </MainNavigator.Navigator>
+     </NavigationContainer>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
